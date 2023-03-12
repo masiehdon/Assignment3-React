94ccaf43af9c4003f1ce2ef988de03405b9a5306
Masih <masihdon@hotmail.de>
2023-03-12 23:21:18 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+      }
+    }
+  }
+})
